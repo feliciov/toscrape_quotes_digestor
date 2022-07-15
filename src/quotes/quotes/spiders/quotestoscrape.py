2d89dd15be2ff5057992f2908e4f20cd27de30d0
Felício <feliciovisnardi@gmail.com>
2022-07-15 19:56:59 +0000

    scrape first page

--- src/quotes/quotes/spiders/quotestoscrape.py
+++ src/quotes/quotes/spiders/quotestoscrape.py
@@ -7,4 +7,10 @@ class QuotestoscrapeSpider(scrapy.Spider):
     start_urls = ['http://quotes.toscrape.com/']
 
     def parse(self, response):
-        pass
+        for quote in response.css('div.quote'):
+            yield {
+                "quotation": quote.css('span.text::text').get(),
+                "author": quote.css("small.author::text").get(),
+                "author_link":quote.css("small.author+a[href]").get(),
+                "keywords":quote.css("div.tags").css("a::text").getall(),
+            }
